402f6d4a57d1563f96056458d30000dcb43e220d
shady <shadysmobarak@gmail.com>
2021-01-09 19:05:30 +0200

    projects and skills animation

--- index.js
+++ index.js
@@ -8,3 +8,20 @@ function showPage(){
     document.getElementById('spin').style.display = 'none';
     document.getElementById('content').style.display = 'block'
 }
+
+let skills = document.getElementById('skills').children;
+
+    setInterval(() => {
+    
+        let random =Math.floor( Math.random() * skills.length )
+
+        Array.from(skills).forEach(skill => {
+            if(skill.classList.contains('skill')){
+                skill.classList.remove('skill')
+            }
+        });
+        if(!skills[random].classList.contains('skill')){
+            skills[random].classList.add('skill')
+        }
+
+    }, 2000);
