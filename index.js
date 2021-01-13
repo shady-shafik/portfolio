fd5cffbb2f6bdb7a5dfa19d3de8311efe0b4b325
shady <shadysmobarak@gmail.com>
2021-01-13 17:11:40 +0200

    projects and some anim

--- index.js
+++ index.js
@@ -1,27 +1,54 @@
-
 function myLoader(){
 
     setTimeout(showPage , 3000)
 }
 
+
+// loading anim
 function showPage(){
-    document.getElementById('spin').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
     document.getElementById('content').style.display = 'block'
 }
 
-let skills = document.getElementById('skills').children;
+// let skills = document.getElementById('skills').children;
 
-    setInterval(() => {
+//     setInterval(() => {
     
-        let random =Math.floor( Math.random() * skills.length )
+//         let random =Math.floor( Math.random() * skills.length )
 
-        Array.from(skills).forEach(skill => {
-            if(skill.classList.contains('skill')){
-                skill.classList.remove('skill')
-            }
-        });
-        if(!skills[random].classList.contains('skill')){
-            skills[random].classList.add('skill')
-        }
+//         Array.from(skills).forEach(skill => {
+//             if(skill.classList.contains('skill')){
+//                 skill.classList.remove('skill')
+//             }
+//         });
+//         if(!skills[random].classList.contains('skill')){
+//             skills[random].classList.add('skill')
+//         }
 
-    }, 2000);
+//     }, 2000);
+
+
+    var typed = new Typed('#typed', {
+        stringsElement: '#typed-strings',
+        loop: 0,
+        typeSpeed: 200 ,
+        showCursor: false,
+        smartBackspace: true,
+        backSpeed: 75,
+        backDelay: 2000,
+  
+      });
+
+        
+    var typed = new Typed('#projects', {
+        stringsElement: '#projects-strings',
+        loop: 0,
+        typeSpeed: 200 ,
+        showCursor: false,
+        smartBackspace: true,
+        backSpeed: 75,
+        backDelay: 2000,
+  
+      });
+
+        
